577697d4d23fcbcea644721b8cdb98537e1f1b98
Tristan <tristanbeedell@gmail.com>
2018-12-23 00:29:14 +0000

    adding people works a bit better

--- modules/discord.js
+++ modules/discord.js
@@ -1,6 +1,5 @@
 module.exports = { initBot, getBot }
 
-const colors = require('colors');
 const Discord = require("discord.js");
 const assert = require('assert');
 const token = process.env.DISCORD_BOT_SECRET;
@@ -8,10 +7,12 @@ const token = process.env.DISCORD_BOT_SECRET;
 let bot;
 
 function initBot(callback, _token) {
+	console.time('bot connection time');
 	bot = new Discord.Client();
 	bot.login(_token || token).catch(console.error);
 	bot.on("ready", () => {
 		console.warn(`connected to '${bot.user.tag}' on discord`.yellow);
+		console.timeEnd('bot connection time');
 		callback(bot)
 	});
 }
