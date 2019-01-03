4f709406a597c4d54d0debc57409d1d166720870
Tristan <tristanbeedell@gmail.com>
2019-01-03 05:18:52 +0000

    fixed guild not found bug of get profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -5,25 +5,28 @@ const { getBot } = require('../modules/discord')
 function get(req, res) {
 	const bot = getBot();
 	// if the guild does not exist, return a 404 error
-	// FIXME: handle guild not found error
-	const guild = bot.guilds.find(guild => guild.name == req.params.guildName.replace(/_/g, ' '));
-	const member = guild.members.find(member => member.displayName == req.params.memberName.replace(/_/g, ' '))
-	if (!guild || !member) {
+	function unavaliable() {
 		res.status(404).render("pages/unavaliable", {
 			session: req.session,
 			bot,
 			redirect: req.url
 		});
+	}
+	
+	const guild = bot.guilds.find(guild => guild.name == req.params.guildName.replace(/_/g, ' '));
+	if (!guild) {
+		unavaliable()
+		return;
+	}
+	const member = guild.members.find(member => member.displayName == req.params.memberName.replace(/_/g, ' '))
+	if (!member) {
+		unavaliable()
 		return;
 	}
 
 	// if the user is not logged into an account in this guild, send an unauthorised error
 	if (!req.session.user || !guild.members.get(req.session.user.member_id)) {
-		res.status(401).render("pages/unavaliable", {
-			session: req.session,
-			bot,
-			redirect: req.url
-		});
+		unavaliable();
 		return;
 	}
 
