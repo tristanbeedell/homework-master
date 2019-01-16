43c9b2c897e1202ccce6fb10b8845aee5db025cd
Tristan <tristanbeedell@gmail.com>
2019-01-16 23:40:07 +0000

    Added force signup command
    If an owner creates a group, they must trigger the bot to add them themself.

--- bot/createUser.js
+++ bot/createUser.js
@@ -1,6 +1,7 @@
 const RichEmbed = require('discord.js').RichEmbed;
 const path = require('path');
 const pool = require(path.join(__dirname, '../modules/database')).getDB();
+const bot = require(path.join(__dirname, '../modules/discord')).getBot();
 const respond = require(path.join(__dirname, './commands.js'));
 const bcrypt = require("bcrypt");
 const urlname = process.env.WEBSITE_URL;
@@ -55,7 +56,7 @@ function sendSetupLink(msg, group) {
 	msg.channel.send(embed);
 }
 
-function dmRespond(msg) {
+async function dmRespond(msg) {
 	if (membersAwaiting.includes(msg.author.id)) {
 		checkMemberFromSchool(msg, (valid, group) => {
 			if (valid) {
@@ -67,8 +68,33 @@ Make sure you send the exact pin and nothing else in the message. It **is** case
 Please try again...`)
 			}
 		})
+	} else if (msg.content.match(/^\s*sign\s?up/)) {
+		if (await signedUp(msg.author)){
+			msg.channel.send('You\'re already signed up.');
+			membersAwaiting = membersAwaiting.filter(id => id !== msg.author.id);
+			return;
+		}
+		const guild = bot.guilds.find(guild => guild.name === msg.content.match(/^\s*sign\s?up\s*(.*)\s*$/)[1]);
+		if (!guild) {
+			msg.channel.send('**Invalid group.** Give a group as follows: `signup Group Name`');
+			return;
+		}
+		const member = guild.members.get(msg.author.id);
+		if (!member) {
+			msg.channel.send('You are not in this server.');
+			return;
+		}
+		newMember(member);
 	} else {
 		let tokens = msg.content;
 		respond({ msg, tokens });
 	}
+}
+
+async function signedUp(member) {
+	const res = await pool.query(`
+		SELECT * FROM users WHERE member_id = '${member.id}' AND complete = TRUE;
+	`).catch(console.error);
+	const rows = res.rows;
+	return rows.length > 0;
 }
